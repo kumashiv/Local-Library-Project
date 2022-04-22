6fdf2791eb667d8a91897ef9fbd382a2ed3ecdd9
Shiv Kumar <kumarshiv_sk@outlook.com>
2022-04-21 21:16:16 -0400

    Models added

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -18,4 +18,30 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # path('catalog/', include('catalog.urls'))
 ]
+
+
+
+from django.urls import include
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+
+
+
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+]
+
+
+
+# Use static() to add URL mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
